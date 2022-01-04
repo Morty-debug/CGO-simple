2f766a4cdf6cfc4d8272acda1ecf501a99c5e135
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-01-04 11:39:43 -0600

    Update compilar.bat

--- compilar.bat
+++ compilar.bat
@@ -2,3 +2,4 @@ gcc -fPIC ./include/saludo.c -shared -o ./lib/libsaludo.dll
 gcc -fPIC ./include/saludo.c -shared -o ./bin/libsaludo.dll
 go build main.go
 copy main.exe bin\main.exe
+del /f main.exe
