548abdfbcf94770929ae814e49f6f6aaa19c8873
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-01-13 07:16:49 -0600

    Delete include directory

--- include/saludo.c
+++
@@ -1,5 +0,0 @@
-#include "saludo.h"
-#include <stdio.h>
-void saludo(const char *nombre){
-	printf("Hola %s desde C\n", nombre);
-}
--- include/saludo.h
+++
@@ -1,4 +0,0 @@
-#ifndef ___SALUDO___
-#define __SALUDO___
-void saludo(const char *nombre);
-#endif
