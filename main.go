8dd39531683367b8446534bc8c992f319f83070f
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-01-13 07:17:36 -0600

    Add files via upload

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 // Las lineas que contienen # no son comentarios, pero esta linea SI
-package main  
-/* 
-#cgo  CFLAGS:  -I./include 
-#cgo  LDFLAGS:  -L./lib  -lsaludo 
-#include "saludo.h" 
-*/  
+package main
+/*
+#include <stdio.h>
+void saludo(const char *nombre){
+	printf("Hola %s desde C\n", nombre);
+}
+*/
 import "C"  
 func main(){
 	nombre := C.CString("Ricky")
